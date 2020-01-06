1320ddf5292ba941d618927b7e7e52a1a64d575c
CircuitWorks1 <47468054+CircuitWorks1@users.noreply.github.com>
2020-01-05 18:02:28 -0800

    EBR_engine_simulation 2.0.ino
    In this version I have done away with the DACs using the wave table and wrote some basic math equations to generate the proper waveforms. I have also incorporated the serial monitor input. This allows users to input a frequency of their choice, to change the RPM and speed at runtime. Anything below 1000 will be read as a speed frequency and anything above 1000 will be read as an RPM frequency.

--- EBR_engine_simulation.ino
+++ EBR_engine_simulation.ino
@@ -1,72 +1,82 @@
+
 //This is an engine sensor simulator designed for the EBR 1190rx/sx to diagnose a failed ECM.
 //It is designed for a Teensy3.5 board https://www.pjrc.com/store/teensy35.html
 //It's suggested that all testing be done with the ECM out of the bike, or disconnect all
 //additional sensors, injectors and relays. The Author of this code assumes no responsibility
 //or liabilty for any damage to product during the execution of this code.
+long previousMicros = 0;
+long speedMicros = 0;
+long int speedRestart;
+long int speedTime;
+float inputFreq;
+float speedFreq;
+float rpmFreq;
+float rpmTime;
+float rpmRestart;
+unsigned long currentMicros = 0;
 
-#define Num_Samples  49
-#define Num_Waves  33
-#define MaxWaveTypes 4
 int i = 0;
-int wavecnt = 0;
+int x = 0;
+String rpmControl;
 
-
-// Wave table used to generate a wave form that simulates the crank shaft sensor.
-static byte WaveFormTable[MaxWaveTypes][Num_Samples] = {
-   // Sin wave
-   {
-0x82, 0x70, 0x5F, 0x4F, 0x41, 0x34, 0x29, 0x21, 0x1C, 0x19, 0x1A, 0x1D, 0x24, 0x2D, 0x38, 0x46, 
-0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xB9, 0xC7, 0xD2, 0xDB, 0xE2, 0xE5, 0xE6, 0xE3, 0xDE, 0xD6, 
-0xCB, 0xBE, 0xB0, 0xA0, 0x8F 
-   },
-
-   // empty
-   { 
-    0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 
-    0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 
-    0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 
-    0x80
-   },
-   // Sin wave inverted
-   {
-    0x80, 0x8D, 0x99, 0xA6, 0xB2, 0xBD, 0xC7, 0xD0, 0xD7, 0xDD, 0xE2, 0xE5, 0xE6, 0xE5, 0xE3, 0xE0,
-    0xDA, 0xD3, 0xCB, 0xC2, 0xB7, 0xAC, 0xA0, 0x93, 0x86, 0x79, 0x6C, 0x5F, 0x53, 0x48, 0x3D, 0x34, 
-    0x2C, 0x25, 0x1F, 0x1C, 0x1A, 0x19, 0x1A, 0x1D, 0x22, 0x28, 0x2F, 0x38, 0x42, 0x4D, 0x59, 0x66, 
-    0x72
-   }
-};
+//This function takes the serial monitor input, applies it to be used as the simulated crank shaft sensor frequency,
+//then calculates the period of the waveform in microseconds. We then calculate
+//the microseconds needed to generate 34 wave forms, and 36 wave forms. This is used to properly simulate
+//the step pattern of the crank shaft(34 steps, then a blank spot the equivalent to 2 steps.)
+void rpmFunction() {
+  float rpmTime = 1 / rpmFreq * 1000000 * 34;
+  float rpmRestart = 1 / rpmFreq * 1000000 * 36;
+  if (currentMicros - previousMicros < rpmTime) {
+    tone(5, rpmFreq);
+  }
+  if (currentMicros - previousMicros > rpmTime) {
+    noTone(5);
+  }
+  if (currentMicros - previousMicros > rpmRestart) {
+    previousMicros = currentMicros;
+  }
+}
+//This function takes the serial monitor input, applies it to be used to simulate the speed sensor frequency,
+//it then, calculates the period of the waveform in microseconds,and genarates a square wave, by
+//rapidly switching a pin between high and low.
+void speedFunction() {
+  float speedTime = 1 / speedFreq * 1000000;
+  float speedRestart = speedTime * 2;
+  if (currentMicros - speedMicros < speedTime) {
+    digitalWrite(35, HIGH);
+  }
+  if (currentMicros - speedMicros > speedTime) {
+    digitalWrite(35, LOW);
+  }
+  if (currentMicros - speedMicros > speedRestart) {
+    speedMicros = currentMicros;
+  }
+}
 
 void setup() {
-
+  pinMode(35, OUTPUT);
 }
 
 void loop() {
-  //basic square wave generated to simulate speed sensor. First value represents pin the wave is output on
-  //second represents the frequency of the wave. 200hz≈75mph.
-  //wire pin 22 to the white wire of the vehicle speed sensor input.
-  tone(22, 200);
+  currentMicros = micros();
 
-  //assign wavetables to variables
-  byte wave_type = 0; // Sine
-  byte no_wave = 1; // no wave
-  byte wave_type2 = 2; // inverted Sine
-
-  //generate wave form. 24 wave cycles, then blank spot equivalent to 2 wave cycles.
-  //output on A21 and A22. Wire A21 to red/beige on crank shaft sensor input 
-  //and A22 to black/beige on the crank shaft sensor input. It should simulate about 5430 rpm
-  if (wavecnt <= Num_Waves){ 
-  analogWrite(A21, WaveFormTable[wave_type][i]);
-  analogWrite(A22, WaveFormTable[wave_type2][i]);
-  }else if (wavecnt > Num_Waves && wavecnt <= 35) {
-    analogWrite(A21, WaveFormTable[no_wave][i]);
-    analogWrite(A22, WaveFormTable[no_wave][i]); 
-  } else{
-    wavecnt = 0;
+  if (Serial.available()) {
+    inputFreq = Serial.parseInt();
   }
-  i++;
-  if (i >= Num_Samples) {
-    i = 0;
-    wavecnt++;
-    Serial.print(wavecnt); 
-}
+  // If the serial input value is greater than 1000, assign the value as an RPM value,
+  // and continue generating speed waveform at current frequency
+  if (inputFreq > 1000) {
+    rpmFreq = inputFreq;
+    rpmFunction();
+    speedFunction();
+
+  }
+
+  // If the serial input value is less than 1000, assign the value as a speed value,
+  // and continue generating RPM waveform at current frequency
+  if (inputFreq < 1000) {
+    speedFreq = inputFreq;
+    speedFunction();
+    rpmFunction();
+  }
 }
